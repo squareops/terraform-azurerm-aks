2af222acb5a159c202448dca823912e8a859d475
Prajwal <prajwal.akhuj@squareops.com>
2023-07-12 12:17:27 +0530

    added managed node pools features

--- example/complete/main.tf
+++ example/complete/main.tf
@@ -9,7 +9,7 @@ locals {
 
 module "vnet" {
   depends_on  = [azurerm_resource_group.terraform_infra]
-  source      = "git::https://github.com/sq-ia/terraform-azure-vnet.git?ref=release/v1"
+  source      = "git::https://github.com/anoushkaakhourysq/terraform-azure-vnet.git?ref=release/v1"
 
   name                          = local.name
   address_space                 = local.address_space
@@ -30,8 +30,13 @@ module "vnet" {
 }
 
 data "azurerm_key_vault_secret" "ssh_key" {
-  name         = "test-ssh-key"
-  key_vault_id = "/subscriptions/{subscription-id}/resourceGroups/prod-skaf-tfstate-rg/providers/Microsoft.KeyVault/vaults/test-ssh-key-skaf"
+  name         = "atmosly-ssh-public"
+  key_vault_id = "/subscriptions/{subscription-id}/resourceGroups/demo-atomsly-tfstate-rg/providers/Microsoft.KeyVault/vaults/atmosly"
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  enabled   = data.azurerm_key_vault_secret.ssh_key.name == "" && data.azurerm_key_vault_secret.ssh_key.key_vault_id == ""
 }
 
 # There are two types of managed idetities "System assigned" & "UserAssigned". User-assigned managed identities can be used on multiple resources.
@@ -43,7 +48,7 @@ resource "azurerm_user_assigned_identity" "identity" {
 
 module "aks_cluster" {
   depends_on = [module.vnet, azurerm_user_assigned_identity.identity]
-  source     = "git::https://github.com/sq-ia/terraform-azure-aks.git?ref=release/v1"
+  source     = "git::https://github.com/prajwalakhuj/terraform-azure-aks.git?ref=release/v1"
 
   name                               = format("%s-aks", local.name)
   environment                        = local.environment
@@ -62,9 +67,7 @@ module "aks_cluster" {
   default_agent_pool_count           = "1"
   default_agent_pool_size            = "Standard_DS2_v2"
   host_encryption_enabled            = false
-  default_node_labels                = { Infra-Services = "true" }
-  managed_agent_pool_count           = "0"  # number of managed node pools to be created.
-  managed_agent_pool_size            = "Standard_DS2_v2"  # Agent size to mapped to the managed node pools.
+  default_node_labels                = { Addon-Services = "true" }
   os_disk_size_gb                    = "30"
   auto_scaling_enabled               = true
   agents_min_count                   = "1"
@@ -73,12 +76,12 @@ module "aks_cluster" {
   agents_availability_zones          = ["1", "2", "3"] # Does not applies to all regions please verify the availablity zones for the respective region.
   rbac_enabled                       = true
   oidc_issuer_enabled                = true
-  open_service_mesh_enabled          = false   # Add on for the open service mesh (istio)
+  open_service_mesh_enabled          = false  # Add on for the open service mesh (istio)
   private_cluster_enabled            = false  # AKS Cluster endpoint access, Disable for public access
   sku_tier                           = "Free"
   subnet_id                          = module.vnet.private_subnets
   admin_username                     = "azureuser"  # node pool username
-  public_ssh_key                     = data.azurerm_key_vault_secret.ssh_key.value
+  public_ssh_key                     = data.azurerm_key_vault_secret.ssh_key.value != "" ? data.azurerm_key_vault_secret.ssh_key.value : tls_private_key.key.public_key_openssh
   agents_type                        = "VirtualMachineScaleSets"  # Creates an Agent Pool backed by a Virtual Machine Scale Set.
   net_profile_outbound_type          = "loadBalancer"   # The outbound (egress) routing method which should be used for this Kubernetes Cluster. Possible values are loadBalancer and userDefinedRouting. Defaults to loadBalancer.
   log_analytics_workspace_sku        = "PerGB2018" # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing
@@ -86,34 +89,34 @@ module "aks_cluster" {
   control_plane_logs_scrape_enabled  = true # Scrapes logs of the aks control plane
   control_plane_monitor_name         = format("%s-%s-aks-control-plane-logs-monitor", local.name, local.environment) # Control plane logs monitoring such as "kube-apiserver", "cloud-controller-manager", "kube-scheduler"
   additional_tags                    = local.additional_tags
-# Managed node pool App
-  create_managed_node_pool_app       = true
-  managed_node_pool_app_name         = "app"
-  managed_node_pool_app_size         = "Standard_DS2_v2"
-  auto_scaling_app_enabled           = true
-  agents_count_app                   = "1"
-  agents_min_count_app               = "1"
-  agents_max_count_app               = "3"
-  agents_availability_zones_app      = ["1", "2"]
-  node_labels_app                    = { App-Services = "true" }
-# Managed node pool Monitor
-  create_managed_node_pool_monitor   = false
-  managed_node_pool_monitor_name     = "monitor"
-  managed_node_pool_monitor_size     = "Standard_DS2_v2"
-  auto_scaling_monitor_enabled       = true
-  agents_count_monitor               = "1"
-  agents_min_count_monitor           = "1"
-  agents_max_count_monitor           = "3"
-  agents_availability_zones_monitor  = ["1", "2"]
-  node_labels_monitor                = { Monitor-Services = "true" }
-# Managed node pool Database
-  create_managed_node_pool_database  = false
-  managed_node_pool_database_name    = "database"
-  managed_node_pool_database_size    = "Standard_DS2_v2"
-  auto_scaling_database_enabled      = true
-  agents_count_database              = "1"
-  agents_min_count_database          = "1"
-  agents_max_count_database          = "3"
-  agents_availability_zones_database = ["1", "2"]
-  node_labels_database               = { Database-Services = "true" }
+}
+
+module "aks_managed_node_pool" {
+  depends_on = [module.aks_cluster]
+  source     = "../../modules/managed_node_pools"
+
+  resource_group_name   = azurerm_resource_group.terraform_infra.name
+  orchestrator_version  = local.k8s_version
+  location              = azurerm_resource_group.terraform_infra.location
+  vnet_subnet_id        = module.vnet.private_subnets
+  kubernetes_cluster_id = module.aks_cluster.kubernetes_cluster_id
+  node_pools = {
+    app = {
+      vm_size                  = "Standard_DS2_v2"
+      auto_scaling_enabled     = true
+      os_disk_size_gb          = 50
+      os_disk_type             = "Managed"
+      node_count               = 1
+      min_count                = 1
+      max_count                = 2
+      availability_zones       = ["1", "2", "3"]
+      enable_node_public_ip    = false # if set to true node_public_ip_prefix_id is required
+      node_public_ip_prefix_id = ""
+      node_labels              = { "App-service" = "true" }
+      node_taints              = ["workload=example:NoSchedule"]
+      host_encryption_enabled  = false
+      max_pods                 = 50
+      agents_tags              = local.additional_tags
+    },
+ }
 }
